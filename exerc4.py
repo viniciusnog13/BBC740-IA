5228d8086ee34cbd5c07b66e8d1f22b24dd9f5d3
viniciusnog13 <vinicius.nogueira@aluno.ufop.edu.br>
2021-05-30 17:11:44 -0300

    Create exerc4.py

---
+++ exerc4.py
@@ -0,0 +1,10 @@
+L = [5, 7, 2, 9, 4, 1, 3]
+print("Lista = ",L)
+print("O tamanho da lista é ",len(L))
+print("O maior elemento da lista é ",max(L))
+print("O menor elemento da lista é ",min(L))
+print("A soma dos elementos da lista é ",sum(L))
+L.sort()
+print("Lista em ordem crescente: ",L)
+L.reverse()
+print("Lista em ordem decrescente: ",L)
