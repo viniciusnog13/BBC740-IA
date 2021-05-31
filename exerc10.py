d01d02bf7c9f92bd20834560cfb8bdc02c89c676
viniciusnog13 <vinicius.nogueira@aluno.ufop.edu.br>
2021-05-31 11:19:34 -0300

    Create exerc10.py

---
+++ exerc10.py
@@ -0,0 +1,4 @@
+sentence = "Practice Problems to Drill List Comprehension in Your Head."
+print (sentence.count(" "))
+soma = [ + 1 for i in sentence if i==" "]
+print ("Número de espaços na string: ",sum(soma))
