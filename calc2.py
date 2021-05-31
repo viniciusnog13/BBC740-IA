6bd219ad3282adb30cd4860d6280570cfc489ca2
viniciusnog13 <vinicius.nogueira@aluno.ufop.edu.br>
2021-05-31 11:19:31 -0300

    Create calc2.py

---
+++ calc2.py
@@ -0,0 +1,9 @@
+class Calculadora: 
+    def soma(self, a, b):
+        return a + b
+    def subtrai(self, a, b): 
+        return a - b
+    def multiplica(self, a, b): 
+        return a * b 
+    def divide(self, a, b):
+        return a / b 
