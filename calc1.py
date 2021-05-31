5a8cc641769c9bdbfc6f51a5a67e86073f797fa9
viniciusnog13 <vinicius.nogueira@aluno.ufop.edu.br>
2021-05-31 11:04:57 -0300

    Create calc1.py

---
+++ calc1.py
@@ -0,0 +1,12 @@
+class CalculadoraSimples: 
+    def __init__(self,a,b):
+        self.a = a 
+        self.b = b 
+    def soma(self):
+        return self.a + self.b
+    def subtrai(self): 
+        return self.a - self.b
+    def multiplica(self): 
+        return self.a * self.b 
+    def divide(self):
+        return self.a / self.b 
